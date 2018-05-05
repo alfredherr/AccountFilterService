03038b9682d57ee6a2f7ac92353bbc986e981e31
Alfredo Herrera <alfredherr@gmail.com>
2018-05-05 12:42:11 -0700

    adding https://loader.io/targets/a97bbb627270683cbd14327a9aae0fd6/verification?first=1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Account_Code_Filter_Service.BusinessLogic;
+using Account_Code_Filter_Service.Models;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Logging;
+
+namespace Account_Code_Filter_Service.Controllers
+{
+    [Route("/x")]
+    public class HomeController
+    {
+
+        public string GetFile()
+        {
+            return "Hellllooooo";
+
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -49,6 +49,17 @@ namespace Account_Code_Filter_Service
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Serve the files Default.htm, default.html, Index.htm, Index.html
+            // by default (in this order), i.e., without having to explicitly qualify the URL.
+            // For example, if your endpoint is http://localhost:3012/ and wwwroot directory
+            // has Index.html, then Index.html will be served when someone hits
+            // http://localhost:3012/
+            app.UseDefaultFiles(); 
+
+            // Enable static files to be served. This would allow html, images, etc. in wwwroot
+            // directory to be served. 
+            app.UseStaticFiles();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
